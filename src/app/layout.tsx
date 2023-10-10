e38cf4bf2359fd2c475bf523dcbd845f1f5c3b7b
Alparslan Korkmaz <95434744+alparslankorkmaz@users.noreply.github.com>
2023-10-10 22:13:24 +0100

    add custom cookie banner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Gantari } from "next/font/google";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import Script from "next/script";
+import CookieConsentBanner from "@/components/CookieBanner";
 
 const gantari = Gantari({ subsets: ["latin"] });
 
@@ -19,19 +19,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <Script
-          id="CookieDeclaration"
-          src="https://consent.cookiebot.com/c2a13ed0-6e14-4576-874a-11501ad56c00/cd.js"
-          type="text/javascript"
-          async
-        />
-      </head>
       <body className={gantari.className}>
         <main>
           <Header />
           {children}
           <Footer />
+          <CookieConsentBanner />
         </main>
       </body>
     </html>
---
+++ src/components/CookieBanner.tsx
@@ -0,0 +1,52 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import cookie from "js-cookie";
+
+const CookieConsentBanner = () => {
+  const [showBanner, setShowBanner] = useState(false);
+
+  useEffect(() => {
+    const consentCookie = cookie.get("cookieConsent");
+
+    if (!consentCookie) {
+      setShowBanner(true);
+    }
+  }, []);
+
+  const handleAccept = () => {
+    setShowBanner(false);
+    cookie.set("cookieConsent", "accepted", { expires: 365 });
+  };
+
+  const handleReject = () => {
+    setShowBanner(false);
+    cookie.set("cookieConsent", "rejected", { expires: 365 });
+  };
+
+  if (!showBanner) {
+    return null;
+  }
+
+  return (
+    <div className="flex flex-wrap lg:gap-5 justify-center items-center sticky bottom-0 z-20 bg-hub_bg bg-bottom text-white p-3 text-center">
+      <p>This website uses cookies to improve your browsing experience.</p>
+      <p>Please click Accept to continue.</p>
+      <div>
+        <button
+          className="font-bold py-2 px-8 border-2 border-slate-400 rounded-xl m-5 hover:bg-red-700 duration-200"
+          onClick={handleReject}
+        >
+          Reject
+        </button>
+        <button
+          className="text-white font-bold py-2 px-8 bg-green-700 rounded-xl m-5"
+          onClick={handleAccept}
+        >
+          Accept
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CookieConsentBanner;
