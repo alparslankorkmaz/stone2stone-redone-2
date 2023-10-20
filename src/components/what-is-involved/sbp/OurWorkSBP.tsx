694d673b8833256cdc31187a9681f61c2dfa3358
Alparslan Korkmaz <95434744+alparslankorkmaz@users.noreply.github.com>
2023-10-20 15:08:33 +0100

    update ourwork component to list latest work

--- src/components/what-is-involved/sbp/OurWorkSBP.tsx
+++ src/components/what-is-involved/sbp/OurWorkSBP.tsx
@@ -26,38 +26,43 @@ export default function OurWork() {
           </Link>
         </div>
         <div className="grid grid-cols-2 lg:grid-cols-3 gap-5">
-          {featuredStudies.slice(0, 3).map((study) => {
-            const { id, link, img, logo, name, tag } = study;
-            return (
-              <li key={id} className="w-full flex justify-center items-center">
-                <Link href={link}>
-                  <div className="relative group w-44 h-44 md:w-72 md:h-72 lg:w-96 lg:h-96">
-                    <img
-                      src={`/img/case-studies/${img}.webp`}
-                      alt="thumbnail"
-                      className="object-cover w-full h-full rounded-xl"
-                    />
-                    <div className="flex justify-center items-center opacity-0 bg-[url('/img/our-work/hover.webp')] rounded-xl bg-center bg-cover ease-in-out duration-300 group-hover:opacity-100 absolute top-0 left-0 h-full w-full"></div>
-                    <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center opacity-0 hover:opacity-100 ease-in-out duration-300">
-                      <div className="text-white flex flex-col h-full text-center justify-center items-center p-5">
-                        <div className="flex justify-center items-center w-28 h-auto">
-                          <img
-                            src={`/img/our-clients/white-logos/${logo}.png`}
-                            alt="company logo"
-                            className="my-5 w-full h-full object-contain"
-                          />
+          {featuredStudies
+            .slice(Math.max(featuredStudies.length - 3, 0))
+            .map((study) => {
+              const { id, link, img, logo, name, tag } = study;
+              return (
+                <li
+                  key={id}
+                  className="w-full flex justify-center items-center"
+                >
+                  <Link href={link}>
+                    <div className="relative group w-44 h-44 md:w-72 md:h-72 lg:w-96 lg:h-96">
+                      <img
+                        src={`/img/case-studies/${img}.webp`}
+                        alt="thumbnail"
+                        className="object-cover w-full h-full rounded-xl"
+                      />
+                      <div className="flex justify-center items-center opacity-0 bg-[url('/img/our-work/hover.webp')] rounded-xl bg-center bg-cover ease-in-out duration-300 group-hover:opacity-100 absolute top-0 left-0 h-full w-full"></div>
+                      <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center opacity-0 hover:opacity-100 ease-in-out duration-300">
+                        <div className="text-white flex flex-col h-full text-center justify-center items-center p-5">
+                          <div className="flex justify-center items-center w-28 h-auto">
+                            <img
+                              src={`/img/our-clients/white-logos/${logo}.png`}
+                              alt="company logo"
+                              className="my-5 w-full h-full object-contain"
+                            />
+                          </div>
+                          <p className="lg:mb-5 font-semibold text-lg lg:text-4xl">
+                            {name}
+                          </p>
+                          <p className="lg:my-5 text-sm lg:text-2xl">{tag}</p>
                         </div>
-                        <p className="lg:mb-5 font-semibold text-lg lg:text-4xl">
-                          {name}
-                        </p>
-                        <p className="lg:my-5 text-sm lg:text-2xl">{tag}</p>
                       </div>
                     </div>
-                  </div>
-                </Link>
-              </li>
-            );
-          })}
+                  </Link>
+                </li>
+              );
+            })}
         </div>
       </div>
     </>
